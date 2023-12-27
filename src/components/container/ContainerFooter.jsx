bde5c1899a2094035a6d05c6265bc151bfabe734
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-12-27 18:15:22 -0300

    footer dinamico

--- src/components/container/ContainerFooter.jsx
+++ src/components/container/ContainerFooter.jsx
@@ -1,26 +1,34 @@
 "use client"
 
+import { useContext, useEffect, useState } from "react";
+import { FormContext } from "@/context/FormContext";
 import { getDataCache } from "@/helpers/getInfoTest";
 import Footer from "../footer/Footer";
 import { Loader } from "../loading";
-import { useEffect, useState } from "react";
 
 
 export const ContainerFooter = () => {
-
+  const { token } = useContext(FormContext)
   const [dataSite, setDataSite] = useState([]);
 
   useEffect(() => {
-    const getDataSite = async () => {  
-      const info = await getDataCache( `https://testapi.tuentrada.com/api/v1/site/ayuda.tuentrada.com`, "12707|5n4wj2vZHLfXa8DcSTqW0dZErhDlZpOU5OeAuqQ4" );
-      const data = info?.data?.site;
-      setDataSite(data)
-    };
-    getDataSite()
+    if (token !== '') {
+      console.log('useEffect footer')
+      const getDataSite = async () => {  
+        const info = await getDataCache( `https://testapi.tuentrada.com/api/v1/site/ayuda.tuentrada.com`, token );
+        const data = info?.data?.site;
+        setDataSite(data)
+      };
+      getDataSite()
+      
+    }
     
-  }, []);
+  }, [token]);
 
-  if(dataSite.length === 0) return <Loader />
+  
+ 
+  if(dataSite === undefined) return <Loader />
+  if(dataSite.length === 0 ) return <Loader />
 
   return (
     <>
