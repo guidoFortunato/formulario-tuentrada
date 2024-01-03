a6621007437567c318a49c64a3999ab009d748bc
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-01-03 15:12:31 -0300

    adsense

--- src/components/adsense/Adsense.jsx
+++ src/components/adsense/Adsense.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Adsense = () => {
   return (
-    <div className="flex justify-center mb-10">< img className="w-[80%] h-[150px] rounded-lg object-cover border border-gray-200" src="https://www.tuentrada.com/images/1703280825.webp" alt="" /></div>
+    <div className="flex justify-center mb-10">< img className="w-[80%]  h-[150px] rounded-lg object-cover border border-gray-200" src="https://www.tuentrada.com/images/1703280825.webp" alt="" /></div>
   )
 }
