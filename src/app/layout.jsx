34eb3425f6d774becd9b9730acd84fadc335f1ed
guido fortunato <guido_fortunato@hotmail.com.ar>
2024-01-05 18:01:54 -0300

    analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,11 +29,11 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="es">
-      <GoogleAnalytics id="G-K2KT0GJC8Q" />
       <body
         className={`${inter.className} flex flex-col min-h-[100vh] m-0`}
         suppressHydrationWarning={true}
       >
+        <GoogleAnalytics />
         <FormProvider>
           <ContainerApp>{children}</ContainerApp>
           <CookieBanner />
