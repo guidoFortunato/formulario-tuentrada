28ed7b6f860c3c36eb76f60de24a5bd37b7caab4
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-01-29 16:13:50 -0300

    pasé a api, modifiqué cookies y activé google adsense optimicé footer

--- src/components/analytics/CookieBanner.jsx
+++ src/components/analytics/CookieBanner.jsx
@@ -21,11 +21,11 @@
 //   }, [cookieConsent]);
 //   return (
 //     <div
-//       className={`my-10 mx-auto max-w-max md:max-w-screen-sm 
-//                         fixed bottom-0 left-0 right-0 
+//       className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+//                         fixed bottom-0 left-0 right-0
 //                         ${
 //                           cookieConsent !== null ? "hidden" : "flex"
-//                         } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+//                         } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4
 //                          bg-white rounded-lg shadow border border-gray-300`}
 //     >
 //       <div className="text-center text-sm">
@@ -44,7 +44,7 @@
 //           onClick={() => setCookieConsent(false)}
 //         >
 //          Personalizar
-//         </button>  
+//         </button>
 //         <button
 //           className="w-auto mr-2 text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark  font-medium rounded-md text-sm px-5 py-2.5 text-center text-sm"
 //           onClick={() => setCookieConsent(true)}
@@ -62,7 +62,6 @@ import Link from "next/link";
 import { getLocalStorage, setLocalStorage } from "@/helpers/storageHelper";
 
 export default function CookieBanner() {
-  // Estados para el ok de cookies y modal
   const [cookieConsent, setCookieConsent] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
@@ -70,13 +69,12 @@ export default function CookieBanner() {
   const initialAcceptedCategories = {
     required: true,
     functional_analytical: true,
-    advertising: true, // Marcado por defecto
-    performance: true, // Marcado por defecto
+    advertising: true,
+    performance: true,
   };
 
   const [acceptedCategories, setAcceptedCategories] = useState(initialAcceptedCategories);
 
-  // Efecto para manejar el ok de cookies
   useEffect(() => {
     const newValue = cookieConsent ? "granted" : "denied";
 
@@ -87,40 +85,38 @@ export default function CookieBanner() {
     setLocalStorage("cookie_consent", cookieConsent);
 
     // Test cookies
-    console.log("Cookie Consent: ", cookieConsent);
+    // console.log("Cookie Consent: ", cookieConsent);
   }, [cookieConsent]);
 
-  // Lista de categorías de cookies
   const cookieCategories = [
     {
       id: "required",
       label: "Cookies requeridas",
       description:
-        "Estas cookies son obligatorias para permitir el funcionamiento principal del sitio web, garantizar que el sitio web sea seguro y permitirnos gestionar la red.",
+        "Estas cookies son necesarias para permitir el funcionamiento principal del sitio web, garantizar que el sitio web sea seguro y permitirnos gestionar la autenticación del usuario.",
     },
     {
       id: "functional_analytical",
       label: "Cookies funcionales y analíticas",
       description:
-        "Estas cookies son funcionales o analíticas. Las cookies funcionales se usan para mejorar lo que puede hacer en nuestro sitio.",
+        "Estas cookies son funcionales o analíticas. Las cookies funcionales se utilizan para mejorar lo que puede hacer en nuestro sitio, mientras que las cookies analíticas nos ayudan a entender cómo los usuarios interactúan con nuestro sitio, permitiéndonos mejorar el contenido y la experiencia del usuario.",
     },
     {
       id: "advertising",
       label: "Cookies publicitarias",
-      description: "Estas cookies se utilizan con fines de marketing.",
+      description: "Estas cookies se utilizan con fines publicitarios, incluyendo Google AdSense.",
     },
     {
       id: "performance",
       label: "Cookies de rendimiento",
       description:
-        "Estas cookies nos permiten contar las visitas y fuentes de circulación para poder medir y mejorar el desempeño de nuestro sitio.",
+        "Estas cookies nos permiten contar las visitas y fuentes de circulación para poder medir y mejorar el rendimiento de nuestro sitio, incluyendo el uso de Google Analytics.",
     },
   ];
 
-  // Manejo de cada categoría
   const handleCategoryToggle = (categoryId) => {
-    if (categoryId === 'required' || categoryId === 'functional_analytical') {
-      return; 
+    if (categoryId === 'required') {
+      return; // Las cookies requeridas deben estar siempre activas
     }
 
     setAcceptedCategories((prevCategories) => ({
@@ -131,10 +127,7 @@ export default function CookieBanner() {
 
   const handleAcceptAll = () => {
     setAcceptedCategories(() => {
-      const allAccepted = {};
-      cookieCategories.forEach((category) => {
-        allAccepted[category.id] = true;
-      });
+      const allAccepted = { ...initialAcceptedCategories };
       return allAccepted;
     });
   };
@@ -153,36 +146,36 @@ export default function CookieBanner() {
         cookieConsent !== null ? "hidden" : "flex"
       } p-4 justify-between items-center bg-white rounded-t-md shadow-md max-w-max md:max-w-screen-sm`}
     >
-      <div className="text-center text-sm text-gray-700">
-        <Link href="/info/cookies">
-          <p>
-            Usamos cookies en este sitio.{" "}
-            <span className="font-semibold text-blue-dark">Más información</span>
-          </p>
-        </Link>
+      <div className="text-center text-xs text-gray-700">
+
+          <p className="text-start">
+            Este sitio utiliza cookies para mejorar su experiencia.
+            <Link href="/info/cookies"> <span className="font-semibold text-blue-dark">Más información</span>     </Link>
+
+   </p>
       </div>
 
       <div className="flex gap-2 text-sm">
         <button
-          className="text-gray-600"
+          className="text-gray-600 text-xs"
           onClick={handleCustomizeClick}
         >
-          Personalizar
+          Personalizar preferencias
         </button>
-     
+
         <button
-          className="w-auto text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark rounded-md px-4 py-2 text-sm"
+          className="w-auto text-white bg-gradient-to-r from-blue-light to-blue-dark hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:blue-dark rounded-md px-4 py-2 text-xs"
           onClick={() => setCookieConsent(true)}
         >
           Aceptar
         </button>
-       
+
       </div>
 
       {showModal && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-md">
-            
+
             <h3 className="text-lg font-semibold mb-4 text-blue-dark">Preferencias de Cookies</h3>
 
             <div className="space-y-4 text-sm">
@@ -195,13 +188,13 @@ export default function CookieBanner() {
                     <input
                       type="checkbox"
                       className={`w-4 h-4 text-blue-dark bg-gray-100 border-gray-300 focus:ring-blue-dark ${
-                        (category.id === 'required' || category.id === 'functional_analytical') && 'text-gray-400 pointer-events-none'
+                        (category.id === 'required') && 'text-gray-400 pointer-events-none'
                       }`}
                       checked={acceptedCategories[category.id]}
                       onChange={() => handleCategoryToggle(category.id)}
                     />
                   </div>
-                  <p className={`text-gray-700 w-[95%] ${(category.id === 'required' || category.id === 'functional_analytical') && 'text-gray-200'}`}>
+                  <p className={`text-gray-700 w-[95%] ${(category.id === 'required') && 'text-gray-200'}`}>
                     {category.description}
                   </p>
                 </div>
--- src/components/footer/Accordion.jsx
+++ src/components/footer/Accordion.jsx
@@ -5,20 +5,21 @@ import { useState } from "react";
 import { animateScroll as scroll } from "react-scroll";
 
 const Accordion = ({ data }) => {
+  // Estado acordeón abierto o cerrado
   const [open, setOpen] = useState(false);
-
   const { pages } = data;
 
+  // Filtrar las páginas que están destinadas al pie de página
   const objectsFooter = pages.filter((page) => page.where === "footer");
 
+  // Obtener los nombres únicos de los tipos de página
   const typeNames = [...new Set(objectsFooter.map((item) => item.type.name))];
 
-  // console.log({ typeNames });
-
+  // Manejar el clic en el botón del acordeón
   const handleClick = () => {
     setOpen((prevState) => !prevState);
 
-    // Si el acordeón se abre (open === true), realiza un desplazamiento suave al final de la página
+    // Si el acordeón se abre, realizar un desplazamiento suave al final de la página
     if (!open) {
       scroll.scrollToBottom({
         duration: 500, // Duración de la animación en milisegundos
@@ -29,44 +30,40 @@ const Accordion = ({ data }) => {
 
   return (
     <div className="accordion flex justify-center flex-col items-center pt-2">
+      {/* Botón del acordeón */}
       <button
-        className="accordion-button py-1 px-7 sm:w-[auto] lg:w-[20%] bg-slate-800 opacity-70 rounded-t-xl text-white active:bg-blue-700 active:opacity-70 "
+        className="accordion-button py-1 px-7 sm:w-[auto] lg:w-[20%] bg-slate-800 opacity-70 rounded-t-xl text-white active:bg-blue-700 active:opacity-70"
         onClick={handleClick}
       >
         {open ? "CERRAR ▲" : "MÁS INFORMACIÓN ▼"}
       </button>
+      {/* Contenido del acordeón */}
       <div
-        className={`accordion-content ${
-          open ? "max-h-screen" : "max-h-0"
-        } transition-max-h ease-in-out duration-500 overflow-hidden w-full ${
+        className={`accordion-content ${open ? "max-h-screen" : "max-h-0"} transition-max-h ease-in-out duration-500 overflow-hidden w-full ${
           open && "border-t border-[#8d858959]"
         }`}
       >
-        <div
-          className={`accordion-content ${
-            open ? "max-h-screen" : "max-h-0"
-          } transition-max-h ease-in-out duration-500 overflow-hidden w-full border-t border-[#8d858959]`}
-        >
-          <div className="py-5 border-[#8d858959] flex justify-evenly flex-col md:flex-row text-gray-200 mb-2 text-lg font-semibold">
-            {typeNames.map((item) => (
-              <div className="text-center mt-5 md:mt-0" key={item}>
-                <p>{item}</p>
-                <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
-                  {pages.map((page) => {
-                    if (item === page.type.name) {
-                      return (
-                        <li key={page.id}>
-                          <Link href={page.path} className="hover:underline ">
-                            {page.title}
-                          </Link>
-                        </li>
-                      );
-                    }
-                  })}
-                </ul>
-              </div>
-            ))}
-          </div>
+        {/* Lista de tipos de páginas con enlaces */}
+        <div className="py-5 border-[#8d858959] flex justify-evenly flex-col md:flex-row text-gray-200 mb-2 text-lg font-semibold">
+          {typeNames.map((item) => (
+            <div className="text-center mt-5 md:mt-0" key={item}>
+              {/* Nombre del tipo de página */}
+              <p>{item}</p>
+              {/* Lista de enlaces para cada página del tipo actual */}
+              <ul className="text-[12px] font-normal leading-relaxed text-gray-400">
+                {pages.map((page) =>
+                  item === page.type.name ? (
+                    <li key={page.id}>
+               
+                      <Link href={page.path} className="hover:underline ">
+                        {page.title}
+                      </Link>
+                    </li>
+                  ) : null
+                )}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </div>
