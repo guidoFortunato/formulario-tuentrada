a2c408c768f226814c23a13c037733a1306b1a37
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-01-31 11:24:51 -0300

    iframe corregido el cartel

--- src/components/main/ArticleIframe.jsx
+++ src/components/main/ArticleIframe.jsx
@@ -9,10 +9,10 @@ export const ArticleIframe = ({ itemColumn }) => {
   return (
     <div className="w-full ">
          <h3 className="text-blue-dark font-semibold mb-2">{title}</h3>
-      <div className="relative w-full overflow-hidden flex justify-center items-center">
-        <div className="aspect-w-16 aspect-h-9 ">
+      <div className=" overflow-hidden flex justify-center items-center">
+        
           <div className="w-full h-full mb-5"  dangerouslySetInnerHTML={{ __html: iframe }} />
-        </div>
+       
       </div>
     </div>
   );
