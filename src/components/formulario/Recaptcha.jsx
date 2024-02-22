bb0b7717ba5903f677ab590ab34137f518d965e2
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-02-22 18:09:18 -0300

    error recaptcha

--- src/components/formulario/Recaptcha.jsx
+++ src/components/formulario/Recaptcha.jsx
@@ -2,6 +2,12 @@ import Script from "next/script";
 import React from "react";
 
 export const Recaptcha = () => {
+
+  const handleError = (error)=>{
+    console.log('error')
+    console.log({error})
+  }
+   
   return (
     <>
       <Script
@@ -14,6 +20,7 @@ export const Recaptcha = () => {
         data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_CLOUDFLARE}
         data-callback="javascriptCallback"
         data-language="es"
+        // data-error-callback={ handleError }
       ></div>
     </>
   );
