c86974ef70d195251794d70fe365d8e419d6c415
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-04-05 11:41:10 -0300

    modificación adsense

--- src/components/adsense/Adsense.jsx
+++ src/components/adsense/Adsense.jsx
@@ -10,15 +10,18 @@ const AdSense = () => {
   }, []);
 
   return (
-    <ins
+   <div className="mx-5 mb-5 ">
+     <ins
       className="adsbygoogle"
-      style={{ display: "block", textAlign: "center" }}
+      style={{ display: "block", textAlign: "center", marginBottom:"20px" }}
       data-ad-layout="in-article"
       data-ad-format="fluid"
       data-ad-client="ca-pub-3241865431125040"
       data-ad-slot="4322497970"
     ></ins>
-  );
+   </div>
+);
+
 };
 
 export default AdSense;
