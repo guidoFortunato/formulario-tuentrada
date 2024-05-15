49af3211bba435636901b527bc440f55370864de
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-05-15 16:05:06 -0300

    anuncio publicitario activado

--- src/components/adsense/LayoutAdsense.jsx
+++ src/components/adsense/LayoutAdsense.jsx
@@ -1,12 +1,15 @@
 export const LayoutAdsense = () => {
   return (
-    <ins
-    className="adsbygoogle"
-    style={{ display: "block", textAlign: "center", marginBottom:"20px" }}
-    data-ad-layout="in-article"
-    data-ad-format="fluid"
-    data-ad-client="ca-pub-3241865431125040"
-    data-ad-slot="4322497970"
-  />
+    <>
+      <h2 className="text-center"> Anuncio Publicitario:</h2>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", textAlign: "center", marginBottom: "20px" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-3241865431125040"
+        data-ad-slot="4322497970"
+      />
+    </>
   );
 };
