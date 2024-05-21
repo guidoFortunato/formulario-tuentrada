4cd9b932f896ad1cc5e31fc880cf30ef3f06b8e5
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-05-21 15:24:04 -0300

    recaptcha 2

--- src/components/formulario/Recaptcha.jsx
+++ src/components/formulario/Recaptcha.jsx
@@ -1,43 +1,35 @@
 "use client";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Turnstile } from "@marsidev/react-turnstile";
-import { FormContext } from "@/context/FormContext";
 import { useRouter } from "next/navigation";
 
 export const Recaptcha = () => {
+  const [statusCloud, setStatusCloud] = useState("");
+  const [tokenCloud, setTokenCloud] = useState("");
   const ref = useRef();
-  const { handleTokenCloud, handleStatusCloud, tokenCloud, statusCloud } = useContext(FormContext);
   const router = useRouter();
-  // console.log({statusCloud})
+
+  console.log({ statusCloud });
 
   const handleEvent = async (e, response) => {
+    console.log("entra");
+    console.log({ statusCloud });
+    // console.log({ response, e });
 
-    if (process.env.NEXT_PUBLIC_RECAPTCHA_ACTIVE === "true") {
-      const form = new FormData();
-      form.set("status", e);
-      try {
-        const res = await fetch("/api/recaptcha", {
-          method: "POST",
-          body: form,
-        });
-        console.log({ recaptchaResponse: res});
-      } catch (error) {
-        console.log({ error });
-      }
-    }
-
-    // console.log({ status: response });
-    handleStatusCloud(response);
-    handleTokenCloud(e);
+    setStatusCloud(response);
+    setTokenCloud(e);
   };
 
   useEffect(() => {
     if (tokenCloud !== "" || statusCloud !== "") {
+      console.log({ statusCloud });
       if (statusCloud === "error") {
-        console.log({ statusCloud });
-        router.push(
-          "https://www.tuentrada.com/experiencia/ayuda-consulta/bot.html"
-        );
+        setStatusCloud("error");
+        window.turnstile.reset();
+        // console.log({ statusCloud });
+        // router.push(
+        //   "https://www.tuentrada.com/experiencia/ayuda-consulta/bot.html"
+        // );
         return;
       }
       if (statusCloud === "success") {
@@ -57,17 +49,36 @@ export const Recaptcha = () => {
           }
 
           const { data: dataServer } = await serverValidation.json();
-          // console.log({ dataServer });
+          console.log({ dataServer });
+
+          if (process.env.NEXT_PUBLIC_RECAPTCHA_ACTIVE === "true") {
+            const form = new FormData();
+            form.set("status", dataServer["error-codes"]);
+            try {
+              const res = await fetch("/api/recaptcha", {
+                method: "POST",
+                body: form,
+              });
+              console.log({ recaptchaResponse: res});
+            } catch (error) {
+              console.log({ error });
+            }
+          }
           const { success } = dataServer;
-          // console.log({success})
+          console.log({ success });
           if (!success) {
             // console.log({ dataServer });
+            setStatusCloud("error");
             window.turnstile.reset();
+            // router.push(
+            //   "https://www.tuentrada.com/experiencia/ayuda-consulta/bot.html"
+            // );
             return;
           }
           // console.log({success})
 
-          window.turnstile.remove();
+          // window.turnstile.remove();
+          // setStatusCloud("")
         };
         resultRecaptcha();
       }
@@ -88,7 +99,7 @@ export const Recaptcha = () => {
         theme: "light",
         language: "es",
         refreshExpired: "auto",
-        size: "invisible",
+        size: "normal",
       }}
     />
   );
