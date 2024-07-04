2169017dfb2c3462b1f1fdb45f8be3d4e58eafad
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2024-07-04 11:46:30 -0300

    Permisos para acceder a la cámara

--- src/components/formulario/typeform/TypeFormScanner.jsx
+++ src/components/formulario/typeform/TypeFormScanner.jsx
@@ -10,6 +10,7 @@ export const TypeFormScanner = ({ item }) => {
   const [imageSrc, setImageSrc] = useState(null);
   const [scan, setScan] = useState(false);
   const [facingMode, setFacingMode] = useState("environment");
+  const [cameraError, setCameraError] = useState(false);
 
   function capturePhoto() {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -18,7 +19,10 @@ export const TypeFormScanner = ({ item }) => {
 
   const handleScan = () => {
     setScan((prev) => !prev);
-    if (scan) setImageSrc(null); // Reset image when turning off the camera
+    if (scan) {
+      setImageSrc(null);
+      setCameraError(false);
+    } 
   };
 
   return (
@@ -44,7 +48,7 @@ export const TypeFormScanner = ({ item }) => {
         id={name}
         type="button"
       >
-       Adjunta el frente de tu DNI, tal como se muestra en la imagen:
+        Adjunta el frente de tu DNI, tal como se muestra en la imagen:
         <div className="flex justify-center">
           <img className="w-[350px]" src="https://tuentrada.com/experiencia/ayuda-consulta/dni.png" alt="" />
         </div>
@@ -67,35 +71,61 @@ export const TypeFormScanner = ({ item }) => {
       )}
 
       {scan && !imageSrc && (
-        <>
-          <Webcam
-            className="rounded-lg"
-            ref={webcamRef}
-            height={160}
-            width={720}
-            videoConstraints={{ facingMode: facingMode }}
-          />
-          <div style={{background:"linear-gradient(0deg, rgba(0,0,0,0.99) 0%, rgba(0,0,0,0) 100%)"}} className="flex justify-evenly items-center p-3 relative -top-[72px] rounded-lg">   
-            <button
-              className="w-12"
-              type="button"
-              onClick={() =>
-                setFacingMode((prev) =>
-                  prev === "user" ? "environment" : "user"
-                )
-              }
-            >
-              <span className="whitespace-nowrap"><img src="https://tuentrada.com/experiencia/ayuda-consulta/scan2.svg" alt="" /></span>
-            </button>
-            <button 
-              className="w-12"
-              type="button"
-              onClick={capturePhoto}
-            >
-              <img src="https://tuentrada.com/experiencia/ayuda-consulta/scan3.svg" alt="" />
-            </button>
+        cameraError ? (
+          <div className="text-red-600 text-xs mt-2 flex">
+           <svg
+            className="mr-1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            width="16px"
+            height="16px"
+          >
+            <path
+              fill="#f44336"
+              d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+            />
+            <path
+              fill="#fff"
+              d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828-2.828L29.656,15.516z"
+            />
+            <path
+              fill="#fff"
+              d="M32.484,29.656l-2.828,2.828l-14.14-14.14l2.828-2.828L32.484,29.656z"
+            />
+          </svg>{" "}<span>Debe acceder a los permisos de la cámara para poder escanear su DNI.</span>
           </div>
-        </>
+        ) : (
+          <>
+            <Webcam
+              className="rounded-lg"
+              ref={webcamRef}
+              height={160}
+              width={720}
+              videoConstraints={{ facingMode: facingMode }}
+              onUserMediaError={() => setCameraError(true)}
+            />
+            <div style={{background:"linear-gradient(0deg, rgba(0,0,0,0.99) 0%, rgba(0,0,0,0) 100%)"}} className="flex justify-evenly items-center p-3 relative -top-[72px] rounded-lg">   
+              <button
+                className="w-12"
+                type="button"
+                onClick={() =>
+                  setFacingMode((prev) =>
+                    prev === "user" ? "environment" : "user"
+                  )
+                }
+              >
+                <span className="whitespace-nowrap"><img src="https://tuentrada.com/experiencia/ayuda-consulta/scan2.svg" alt="" /></span>
+              </button>
+              <button 
+                className="w-12"
+                type="button"
+                onClick={capturePhoto}
+              >
+                <img src="https://tuentrada.com/experiencia/ayuda-consulta/scan3.svg" alt="" />
+              </button>
+            </div>
+          </>
+        )
       )}
 
       {imageSrc && (
