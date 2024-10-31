a3489ce34062d5957873c6f605cceb9daeae0bd0
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-10-31 09:09:36 -0300

    agrego verificación de password en redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -3,12 +3,19 @@ import { createClient } from "redis";
 const getRedisClient = async () => {
   if (!globalThis.redisClient) {
     console.log("se crea la conexion a redis por primera vez");
-    globalThis.redisClient = createClient({
+
+    const redisOptions = {
       socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
       },
-    });
+    };
+
+    if (process.env.REDIS_PASSWORD !== "null") {
+      redisOptions.password = process.env.REDIS_PASSWORD;
+    }
+
+    globalThis.redisClient = createClient(redisOptions);
 
     globalThis.redisClient.on("error", (err) => {
       console.log("Redis Client Error", err);
